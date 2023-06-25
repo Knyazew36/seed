f49929764f2b9302c587cd9f41e362cd202ad5a5
Knyazew36 <knyazew.sv@gmail.com>
2023-06-25 12:47:20 +0300

    #up

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,8 @@ AOS.init();
 import './modal.js';
 import './swiper.js';
 
+window.addEventListener('DOMContentLoaded', () => {});
+
 window.addEventListener('scroll', () => {
   let scrollPosition = document.documentElement.scrollTop;
   const header = document.querySelector('.header');
@@ -11,39 +13,38 @@ window.addEventListener('scroll', () => {
   } else {
     header.classList.remove('--active');
   }
-});
+  const videoBtn = document.querySelector('.section-about__button');
+  const video = document.querySelector('#video');
+  let isVideoLoaded = false;
 
-const videoBtn = document.querySelector('.section-about__button');
-const video = document.querySelector('#video');
-let isVideoLoaded = false;
-
-function playVideo() {
-  if (!isVideoLoaded) {
-    video.load();
-    video.setAttribute('preload', 'metadata');
-    video.addEventListener('loadedmetadata', () => {
-      isVideoLoaded = true;
+  function playVideo() {
+    if (!isVideoLoaded) {
+      video.load();
+      video.setAttribute('preload', 'metadata');
+      video.addEventListener('loadedmetadata', () => {
+        isVideoLoaded = true;
+        video.play();
+      });
+    } else {
       video.play();
-    });
-  } else {
-    video.play();
+    }
+    video.setAttribute('controls', '');
+    videoBtn.classList.add('section-about__button_active');
   }
-  video.setAttribute('controls', '');
-  videoBtn.classList.add('section-about__button_active');
-}
 
-function pauseVideo() {
-  videoBtn.classList.remove('section-about__button_active');
-  video.removeAttribute('controls');
-}
-
-videoBtn.addEventListener('click', () => {
-  if (video.paused) {
-    playVideo();
-  } else {
-    video.pause();
-    pauseVideo();
+  function pauseVideo() {
+    videoBtn.classList.remove('section-about__button_active');
+    video.removeAttribute('controls');
   }
+
+  videoBtn.addEventListener('click', () => {
+    if (video.paused) {
+      playVideo();
+    } else {
+      video.pause();
+      pauseVideo();
+    }
+  });
+
+  video.addEventListener('pause', pauseVideo);
 });
-
-video.addEventListener('pause', pauseVideo);
--- javascript/swiper.js
+++ javascript/swiper.js
@@ -1,9 +1,11 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  autoplay: {
-    delay: 2000,
-  },
-  slidesPerView: 'auto',
-  spaceBetween: 10,
-  allowTouchMove: false,
+window.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    allowTouchMove: false,
+  });
 });
